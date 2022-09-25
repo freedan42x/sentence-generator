bc578dff58af55af8190718c6e88c8dc49bd0e44
bredor <danil123550@gmail.com>
2022-09-25 21:30:28 +0300

    a script for adding new words excluding duplicates

---
+++ add_words.py
@@ -0,0 +1,24 @@
+f = open('english', 'r+')
+
+all_words = f.read().split()
+new_words = input('Enter words to add: ').split()
+
+ignored = []
+added = []
+for word in new_words:
+    if word in all_words:
+        ignored.append(word)
+    else:
+        added.append(word)
+
+print('\n')
+
+print('Ignored %d words:' % len(ignored))
+print('\t%s\n' % ' '.join(ignored))
+
+print('Added %d words:' % len(added))
+print('\t%s\n' % ' '.join(added))
+
+f.write(' '.join(all_words + added))
+
+f.close()
